66dabfa3eda89bf50aa54c420dc0e5b653250df5
amirhossein khanmohammadi <amirhossein.kh.job@gmail.com>
2023-09-06 15:48:07 +0330

    #6 add new component and style

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        
+        <form action="">
+         <div>
+            <label >name</label>
+            <input type="text"  />
+        </div>
+
+        <div>
+            <label >phone</label>
+            <input type="tel" />
+        </div>
+        
+        <div>
+            <label >email </label>
+            <input type="email "  />
+        </div>
+
+        <div>
+            <button>Add contact</button>
+        </div>
+            
+        </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewFriend',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import NewFriend from './components/NewFriend.vue'
 import ContactFriends from './components/ContactFriends.vue'
 const app=createApp(App);
 
 
 
 app.component('ContactFriends',ContactFriends);
+app.component('NewFriend',NewFriend);
 app.mount('#app')
